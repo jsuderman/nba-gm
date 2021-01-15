7c7ae6aacc81f960ca15d84d905168b18252e1aa
Justin Suderman <jsuderman1@gmail.com>
2021-01-14 17:46:01 -0800

    added hero section with header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="app">
       <Navigation />
+      <Hero />
+      {/* main body */}
+      {/* api call to get player info */}
+      {/* being able to CRUD players to "your roster" */}
     </div>
   );
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Hero.css"
+
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero__header">
+                <h1>welcome to NBA GM</h1>
+                <p>search for players and choose your team</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
